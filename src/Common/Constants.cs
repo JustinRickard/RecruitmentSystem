b5c01cae5ae8247b69092e6cace2511ea0e358fa
Justin Rickard <justinr86.jr@gmail.com>
2017-04-01 21:17:29 +0100

    Add roles and permissions constants

--- src/Common/Constants.cs
+++ src/Common/Constants.cs
@@ -2,6 +2,52 @@ namespace Common
 {
     public static class Constants
     {
+        public static class Authorization
+        {
+            public static class Roles {
+                public const string SuperAdmin = "SuperAdmin";
+                public const string UserAdmin = "UserAdmin";
+                public const string UserReadOnly = "UserReadOnly";
+                public const string AuditReadonly = "AuditReadonly";
+                public const string ProjectAdmin = "ProjectAdmin";
+                public const string ProjectReadOnly = "ProjectReadOnly";
+                public const string WorkflowAdmin = "WorkflowAdmin";
+                public const string WorkflowReadOnly = "WorkflowReadOnly";
+                public const string WorkflowStepAdmin = "WorkflowStepAdmin";
+                public const string WorkflowStepReadOnly = "WorkflowStepReadOnly";
+            }
+            public static class Permissions
+            {
+                public const string Client_Create = "Client_Create";
+                public const string Client_View = "Client_View";
+                public const string Client_Edit = "Client_Edit";
+                public const string Client_Delete = "Client_Delete";
+                public const string Client_Obliterate = "Client_Obliterate";
+                public const string User_Create = "User_Create";
+                public const string User_View = "User_View";
+                public const string User_Edit = "User_Edit";
+                public const string User_Delete = "User_Delete";
+                public const string User_Obliterate = "User_Obliterate";
+                public const string Project_Create = "Project_Create";
+                public const string Project_View = "Project_View";
+                public const string Project_Edit = "Project_Edit";
+                public const string Project_Delete = "Project_Delete";
+                public const string Project_Obliterate = "Project_Obliterate";
+                public const string Workflow_Create = "Workflow_Create";
+                public const string Workflow_View = "Workflow_View";
+                public const string Workflow_Edit = "Workflow_Edit";
+                public const string Workflow_Delete = "Workflow_Delete";
+                public const string Workflow_Obliterate = "Workflow_Obliterate";
+                public const string WorkflowStep_Create = "WorkflowStep_Create";
+                public const string WorkflowStep_View = "WorkflowStep_View";
+                public const string WorkflowStep_Edit = "WWorkflowStep_Editorkflow_Edit";
+                public const string WorkflowStep_Delete = "WorkflowStep_Delete";
+                public const string WorkflowStep_Obliterate = "WorkflowStep_Obliterate";
+                public const string Workflow_Take = "Workflow_Take";
+
+            }
+        }
+
         public static class Resources {
             public static class Keycodes {
                 public static class Generic 
